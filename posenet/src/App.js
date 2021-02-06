47b5ed43b9a4ce0887035605999394ec5750cb4c
Pathum Nakandala <pathumnakandala@gmail.com>
2021-02-06 19:42:39 +0530

    Detect keypoints, draw keypoints & draw the skeleton on canvas.

--- posenet/src/App.js
+++ posenet/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
+import { drawKeypoints, drawSkeleton } from "./utilities";
 
 
 function App() {
@@ -17,8 +18,51 @@ function App() {
       const net = await posenet.load({
         inputResolution:{width:640, height:480},
         scale:0.5
-      })
+      });
+
+      setInterval(()=>{
+        detect(net)
+      }, 100) //run 
     }
+
+    //Detect function.
+
+    const detect = async (net) => {
+      if(typeof webcamRef.current !== "undefined" && webcamRef.current !== null && 
+      webcamRef.current.video.readyState===4) {
+        
+        //get video properties
+        const video = webcamRef.current.video;
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
+
+        //set video width
+        webcamRef.current.video.width = videoWidth;
+        webcamRef.current.video.height = videoHeight;
+
+        //Make detections
+        const pose = await net.estimateSinglePose(video);
+        console.log(pose);
+
+        //call draw function
+        drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+
+      }
+    };
+
+
+    //Draw function
+    const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+      const ctx = canvas.current.getContext("2d");
+      canvas.current.width = videoWidth;
+      canvas.current.height = videoHeight;
+
+      drawKeypoints(pose["keypoints"], 0.5, ctx);
+      drawSkeleton(pose["keypoints"], 0.5, ctx);
+    };
+
+
+    runPosenet();
 
   return (
     <div className="App">
@@ -38,15 +82,12 @@ function App() {
              }}
              />
 
-            
-
-
 
             <canvas
             ref={canvasRef}
-             style={{
-          position: "absolute",
-           marginLeft: "auto",
+            style={{
+            position: "absolute",
+            marginLeft: "auto",
             marginRight: "auto",
             left: 0,
             right: 0,
