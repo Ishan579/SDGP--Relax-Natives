047e9fdae3d612e2c22d8b7211b2d3cf9e8380f5
SheharaDill <61136284+SheharaDill@users.noreply.github.com>
2021-02-06 16:55:32 +0530

    import dependencies and setup Webcam and canvas

---
+++ posenet/src/App.js
@@ -0,0 +1,47 @@
+import React, {useRef} from 'react';
+import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import Webcam from "react-webcam";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Webcam
+        style={{
+          position: "absolute",
+           marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex:9,
+            width:640,
+            height:480
+             }}
+             />
+
+            
+
+
+
+            <canvas style={{
+          position: "absolute",
+           marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex:9,
+            width:640,
+            height:480
+             }}/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
