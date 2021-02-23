ff548826eaf9f0332c0a648bbe1769a9369a18cd
Buddhima12 <61305612+Buddhima12@users.noreply.github.com>
2021-02-23 13:06:11 +0530

    Update App.js

--- posenet/src/App.js
+++ posenet/src/App.js
@@ -1,3 +1,11 @@
+// 1. Install dependencies DONE
+// 2. Import dependencies DONE
+// 3. Setup webcam and canvas DONE
+// 4. Define references to those DONE
+// 5. Load posenet DONE
+// 6. Detect function DONE
+// 7. Drawing utilities from tensorflow DONE
+// 8. Draw functions DONE
 import React, {useRef} from 'react';
 import './App.css';
 import * as tf from "@tensorflow/tfjs";
