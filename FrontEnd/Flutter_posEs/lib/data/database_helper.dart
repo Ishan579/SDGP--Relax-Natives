6c54ff994b30e5321868f397f51a83f89bf27d80
SheharaDill <61136284+SheharaDill@users.noreply.github.com>
2021-04-12 13:09:06 +0530

    Create Db table, save & delete user

--- FrontEnd/Flutter_posEs/lib/data/database_helper.dart
+++ FrontEnd/Flutter_posEs/lib/data/database_helper.dart
@@ -4,6 +4,8 @@ import 'dart:io' as io;
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
+import '../models/user.dart';
+
 class DatabaseHelper {
   static final DatabaseHelper _instance = new DatabaseHelper.internal();
   factory DatabaseHelper() => _instance;
@@ -26,5 +28,24 @@ class DatabaseHelper {
     String path = join(documentDirectory.path, "main.db");
     var ourDb = await openDatabase(path, version: 1, onCreate: _onCreate);
     return ourDb;
+  }
+
+  void _onCreate(Database db, int version) async {
+    await db.execute(
+        "CREATE TABLE User(id INTEGER PRMARY KEY, username TEXT, password TEXT)");
+    print("Table is created");
+  }
+//insertion
+  Future<int> saveUser(User user) async {
+    var dbClient = await db;
+    int res = await dbClient.insert("User", user.toMap());
+    return res;
+  }
+
+  //deletion
+   Future<int> deleteUser(User user) async {
+    var dbClient = await db;
+    int res = await dbClient.delete("User");
+    return res;
   }
 }
