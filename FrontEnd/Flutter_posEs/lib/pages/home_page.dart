6f70d8e6badf726a508ae5e354110ec7596ade88
Rajitha Abeysinghe <rajiabeysinghe@gmail.com>
2021-04-15 20:12:20 +0530

    Camera Added
    Camera Added to Home Page on home_page.dart.
    Added Image Picker dependency.

--- FrontEnd/Flutter_posEs/lib/pages/home_page.dart
+++ FrontEnd/Flutter_posEs/lib/pages/home_page.dart
@@ -1,15 +1,92 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+
+
 class HomePage extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-        title: new Text("Home"),
-      ),
-      body: new Center(
-        child: new Text("Yaaaaaayyy"),
-      ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
 
-    );;
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  File _image;
+  ImagePicker imagePicker;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    imagePicker=ImagePicker();
+  }
+  void chooseImage() async{
+    PickedFile pickedFile= await imagePicker.getImage(source: ImageSource.gallery);
+    setState(() {
+      _image = File(pickedFile.path);
+    });
+
+
+
+  }
+  void takePicture() async{
+
+    PickedFile pickedFile= await imagePicker.getImage(source: ImageSource.camera);
+    setState(() {
+      _image = File(pickedFile.path);
+    });
+
+
+  }
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _image !=null ? Image.file(_image) :Icon(Icons.image,size:100),
+            RaisedButton(child:Text("Choose/Capture"),onPressed: takePicture,)
+          ],
+        ),
+      ),
+     // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
