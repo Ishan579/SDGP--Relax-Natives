4f8534751a951da103b1cb56d2f3199e4e21e04f
Pathum Nakandala <pathumnakandala@gmail.com>
2021-04-19 16:00:59 +0530

    Implemented all the workout video players.
    Done implementing all the workout video players in video_player page.

--- FrontEnd/Flutter_posEs/lib/pages/video_player.dart
+++ FrontEnd/Flutter_posEs/lib/pages/video_player.dart
@@ -66,3 +66,263 @@ class stretchVideo extends State<stretchPage> {
     );
   }
 }
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+class squatPage extends StatefulWidget {
+  squatPage();
+  @override
+  squatVideo createState() => squatVideo();
+}
+
+class squatVideo extends State<squatPage> {
+  YoutubePlayerController _controller;
+
+  void runYoutubePlayer() {
+    _controller = YoutubePlayerController(
+        initialVideoId: YoutubePlayer.convertUrlToId(
+            'https://www.youtube.com/watch?v=aclHkVaku9U'),
+        flags: YoutubePlayerFlags(
+          enableCaption: false,
+          isLive: false,
+          autoPlay: false,
+        ));
+  }
+
+  @override
+  void initState() {
+    runYoutubePlayer();
+    super.initState();
+  }
+
+  @override
+  void deactivate() {
+    _controller.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller,
+      ),
+      builder: (context, player) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text('How to do Squats'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[player],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+class crunchesPage extends StatefulWidget {
+  crunchesPage();
+  @override
+  crunchesVideo createState() => crunchesVideo();
+}
+
+class crunchesVideo extends State<crunchesPage> {
+  YoutubePlayerController _controller;
+
+  void runYoutubePlayer() {
+    _controller = YoutubePlayerController(
+        initialVideoId: YoutubePlayer.convertUrlToId(
+            'https://www.youtube.com/watch?v=0OxOI3sAIrM'),
+        flags: YoutubePlayerFlags(
+          enableCaption: false,
+          isLive: false,
+          autoPlay: false,
+        ));
+  }
+
+  @override
+  void initState() {
+    runYoutubePlayer();
+    super.initState();
+  }
+
+  @override
+  void deactivate() {
+    _controller.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller,
+      ),
+      builder: (context, player) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text('How To Do Crunches'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[player],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+class burpeesPage extends StatefulWidget {
+  burpeesPage();
+  @override
+  burpeesVideo createState() => burpeesVideo();
+}
+
+class burpeesVideo extends State<burpeesPage> {
+  YoutubePlayerController _controller;
+
+  void runYoutubePlayer() {
+    _controller = YoutubePlayerController(
+        initialVideoId: YoutubePlayer.convertUrlToId(
+            'https://www.youtube.com/watch?v=dZgVxmf6jkA'),
+        flags: YoutubePlayerFlags(
+          enableCaption: false,
+          isLive: false,
+          autoPlay: false,
+        ));
+  }
+
+  @override
+  void initState() {
+    runYoutubePlayer();
+    super.initState();
+  }
+
+  @override
+  void deactivate() {
+    _controller.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller,
+      ),
+      builder: (context, player) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text('How To Do Burpees'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[player],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+class lungesPage extends StatefulWidget {
+  lungesPage();
+  @override
+  lungesVideo createState() => lungesVideo();
+}
+
+class lungesVideo extends State<lungesPage> {
+  YoutubePlayerController _controller;
+
+  void runYoutubePlayer() {
+    _controller = YoutubePlayerController(
+        initialVideoId: YoutubePlayer.convertUrlToId(
+            'https://www.youtube.com/watch?v=QOVaHwm-Q6U'),
+        flags: YoutubePlayerFlags(
+          enableCaption: false,
+          isLive: false,
+          autoPlay: false,
+        ));
+  }
+
+  @override
+  void initState() {
+    runYoutubePlayer();
+    super.initState();
+  }
+
+  @override
+  void deactivate() {
+    _controller.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller,
+      ),
+      builder: (context, player) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text('How To Do Lunges'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[player],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
