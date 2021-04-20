7b21a15fe1cd9615d4ffb4d813cdd37affeb3713
raji-a <rajiabeysinghe@gmail.com>
2021-04-20 16:37:09 +0530

    Revert "Revert "bug fixes on Squat Instructions""
    This reverts commit 2b0ec95898d50f0861c1a5a19d8f83f4aed6a604.

--- FrontEnd/Flutter_posEs/lib/pages/services/render_squat.dart
+++ FrontEnd/Flutter_posEs/lib/pages/services/render_squat.dart
@@ -107,27 +107,7 @@ class _RenderSquatState extends State<RenderSquat> {
         setState(() {
          checkCorrectPosture = false;
           correctColor = Colors.red;
-         //START :Individual Instructions
-         if((shoulderLY>320)){
-           setState(() {
-             whatToDo = "leftShoulder should be "+"less than 320";
-             whatToDo = "leftShoulder is "+ shoulderLY.toString();
-           });
-         }
-         else if ((shoulderRY<320)){
-           whatToDo = "leftShoulder should be "+"less than 320";
-           whatToDo = "leftShoulder is "+ shoulderRY.toString();
-         }
-         else if((kneeRY>570)){
-           whatToDo = "rightKnee should be "+"greater than 570";
-           whatToDo = "rightKnee is "+ kneeRY.toString();
-         }
-         else if((kneeLY>570)){
-           whatToDo = "leftKnee should be "+"greater than 570";
-           whatToDo = "leftKnee is "+ kneeLY.toString();
-         }
 
-          //END of Individual Instructions
 
         });
       }
@@ -137,6 +117,28 @@ class _RenderSquatState extends State<RenderSquat> {
   Future<void> _countingLogic(Map<String, List<double>> poses) async {
     if (poses != null) {
       _checkCorrectPosture(poses);
+      //START :Individual Instructions
+      if((shoulderLY>320)){
+        setState(() {
+          whatToDo = "leftShoulder should be "+"less than 320";
+          whatToDo = "leftShoulder is "+ shoulderLY.toString();
+        });
+      }
+      else if ((shoulderRY<320)){
+        whatToDo = "leftShoulder should be "+"less than 320";
+        whatToDo = "leftShoulder is "+ shoulderRY.toString();
+      }
+      else if((kneeRY>570)){
+        whatToDo = "rightKnee should be "+"greater than 570";
+        whatToDo = "rightKnee is "+ kneeRY.toString();
+      }
+      else if((kneeLY>570)){
+        whatToDo = "leftKnee should be "+"greater than 570";
+        whatToDo = "leftKnee is "+ kneeLY.toString();
+      }
+
+      //END of Individual Instructions
+
 
       if (checkCorrectPosture && squatUp && midCount == false) {
         //in correct initial posture
